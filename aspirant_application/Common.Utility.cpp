a02d5ea39e2c9aaa82eb193c2adae48508e1bfcb
ernestpazera <ernestpazera@gmail.com>
2021-04-18 04:46:34 -0500

    eliminated unused functions from common utility

--- aspirant_application/Common.Utility.cpp
+++ aspirant_application/Common.Utility.cpp
@@ -26,36 +26,5 @@ namespace common::Utility
 	{
 		return value * 100 / maximum;
 	}
-
-	size_t NextIndex(size_t index, size_t count)
-	{
-		if (count > 0)
-		{
-			return ((index + 1) % count);
-		}
-		else
-		{
-			return (0);
-		}
-	}
-
-	size_t PreviousIndex(size_t index, size_t count)
-	{
-		if (count > 0)
-		{
-			return ((index + count - 1) % count);
-		}
-		else
-		{
-			return (0);
-		}
-
-	}
-
-	int PositiveModulo(int dividend, size_t divisor)
-	{
-		dividend %= (int)divisor;
-		return (dividend < 0) ? (dividend + (int)divisor) : (dividend);
-	}
 }
 
--- aspirant_application/Common.Utility.h
+++ aspirant_application/Common.Utility.h
@@ -6,8 +6,5 @@ namespace common::Utility
 	std::vector<std::string> ParseCommandLine(int, char**);
 	int StringToInt(const std::string&);
 	int ToPercentage(int, int);
-	size_t NextIndex(size_t, size_t);
-	size_t PreviousIndex(size_t, size_t);
-	int PositiveModulo(int, size_t);
 }
 
