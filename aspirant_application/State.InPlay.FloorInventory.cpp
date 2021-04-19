a22e67b20ebfdd84cf5b1fe4ff4f7e2d6b826ae1
ernestpazera <ernestpazera@gmail.com>
2021-04-19 18:24:46 -0500

    floorinventory state refactor

--- aspirant_application/State.InPlay.FloorInventory.cpp
+++ aspirant_application/State.InPlay.FloorInventory.cpp
@@ -7,6 +7,7 @@
 #include "Game.World.Items.h"
 #include "Game.Avatar.h"
 #include "Game.Avatar.Items.h"
+#include "Common.Utility.h"
 namespace state::in_play::FloorInventory
 {
 	const std::string LAYOUT_NAME = "State.InPlay.FloorInventory";
@@ -22,43 +23,25 @@ namespace state::in_play::FloorInventory
 		}
 	}
 
+	const std::map<::Command, std::function<void()>> commandHandlers =
+	{
+		{ ::Command::BACK, []() {application::UIState::Write(::UIState::LEAVE_PLAY); }},
+		{ ::Command::PREVIOUS, []() {graphics::FloorInventory::ResetIndex(); application::UIState::Write(::UIState::IN_PLAY_MAP); } },
+		{ ::Command::NEXT, []() {graphics::FloorInventory::ResetIndex(); application::UIState::Write(::UIState::IN_PLAY_INVENTORY); } },
+		{ ::Command::YELLOW, []() {graphics::FloorInventory::ResetIndex(); application::UIState::Write(::UIState::IN_PLAY_INVENTORY); } },
+		{ ::Command::UP, graphics::FloorInventory::PreviousIndex},
+		{ ::Command::DOWN, graphics::FloorInventory::NextIndex},
+		{ ::Command::GREEN, PickUpItem }
+	};
+
 	static void OnCommand(const ::Command& command)
 	{
-		switch (command)
-		{
-		case ::Command::BACK:
-			application::UIState::Write(::UIState::LEAVE_PLAY);
-			break;
-		case ::Command::PREVIOUS:
-			graphics::FloorInventory::ResetIndex();
-			application::UIState::Write(::UIState::IN_PLAY_MAP);
-			break;
-		case ::Command::NEXT:
-		case ::Command::YELLOW:
-			graphics::FloorInventory::ResetIndex();
-			application::UIState::Write(::UIState::IN_PLAY_INVENTORY);
-			break;
-		case ::Command::UP:
-			graphics::FloorInventory::PreviousIndex();
-			break;
-		case ::Command::DOWN:
-			graphics::FloorInventory::NextIndex();
-			break;
-		case ::Command::GREEN:
-			PickUpItem();
-			break;
-		}
-	}
-
-	static void OnUpdate(const Uint32& ticks)
-	{
-
+		common::Utility::Dispatch(commandHandlers, command);
 	}
 
 	void Start()
 	{
 		::application::Command::SetHandler(::UIState::IN_PLAY_FLOOR, OnCommand);
 		::application::Renderer::SetRenderLayout(::UIState::IN_PLAY_FLOOR, LAYOUT_NAME);
-		::application::Update::AddHandler(::UIState::IN_PLAY_FLOOR, OnUpdate);
 	}
 }
