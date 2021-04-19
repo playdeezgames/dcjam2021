d7b26ef23563300f795d53bfd932f371efe89bd9
ernestpazera <ernestpazera@gmail.com>
2021-04-19 14:15:31 -0500

    options is refactored

--- aspirant_application/State.Options.cpp
+++ aspirant_application/State.Options.cpp
@@ -21,6 +21,7 @@ namespace state::Options
 	const std::string MUX_VOLUME_MENU_ITEM_ID = "MuxVolume";
 	const std::string MUTE = "Mute";
 	const std::string UNMUTE = "Unmute";
+	const int VOLUME_DELTA = 8;
 
 	enum class OptionsItem
 	{
@@ -30,13 +31,10 @@ namespace state::Options
 		BACK
 	};
 
-	const int VOLUME_DELTA = 8;
-
 	static void AdjustSfxVolume(int delta)
 	{
 		common::audio::Sfx::SetVolume(common::audio::Sfx::GetVolume() + delta);
 		common::audio::Sfx::Play(application::Sounds::WOOHOO);
-		
 		::Options::Save();
 	}
 
@@ -51,16 +49,18 @@ namespace state::Options
 		return (OptionsItem)graphics::Menus::ReadValue(LAYOUT_NAME, MENU_ID).value();
 	}
 
+	const std::map<OptionsItem, std::function<void(int)>> itemChangers =
+	{
+		{ OptionsItem::SFX_VOLUME, AdjustSfxVolume },
+		{ OptionsItem::MUX_VOLUME, AdjustMuxVolume }
+	};
+
 	static void ChangeItem(int delta)
 	{
-		switch (GetCurrentItem())
+		auto iter = itemChangers.find(GetCurrentItem());
+		if (iter != itemChangers.end())
 		{
-		case OptionsItem::SFX_VOLUME:
-			AdjustSfxVolume(delta);
-			break;
-		case OptionsItem::MUX_VOLUME:
-			AdjustMuxVolume(delta);
-			break;
+			iter->second(delta);
 		}
 	}
 
@@ -74,77 +74,67 @@ namespace state::Options
 		ChangeItem(-VOLUME_DELTA);
 	}
 
+	const std::map<OptionsItem, std::function<void()>> activators =
+	{
+		{ OptionsItem::TOGGLE_MUTE, []() { common::Audio::SetMuted(!common::Audio::IsMuted()); ::Options::Save(); }},
+		{ OptionsItem::BACK, []() { ::application::UIState::Write(::UIState::MAIN_MENU); }}
+	};
+
 	static void ActivateItem()
 	{
-		switch (GetCurrentItem())
+		auto iter = activators.find(GetCurrentItem());
+		if (iter != activators.end())
 		{
-		case OptionsItem::TOGGLE_MUTE:
-			common::Audio::SetMuted(!common::Audio::IsMuted());
-			::Options::Save();
-			break;
-		case OptionsItem::BACK:
-			::application::UIState::Write(::UIState::MAIN_MENU);
-			break;
+			iter->second();
 		}
 	}
+
+	const std::map<::Command, std::function<void()>> commandHandlers =
+	{
+		{::Command::UP, [](){ graphics::Menus::Previous(LAYOUT_NAME, MENU_ID); }},
+		{::Command::DOWN, []() { graphics::Menus::Next(LAYOUT_NAME, MENU_ID); }},
+		{::Command::LEFT, DecreaseItem },
+		{::Command::RIGHT, IncreaseItem },
+		{::Command::BACK, []() { ::application::UIState::Write(::UIState::MAIN_MENU); }},
+		{::Command::RED, []() { ::application::UIState::Write(::UIState::MAIN_MENU); }},
+		{ ::Command::GREEN, ActivateItem }
+	};
 
 	static void OnCommand(const ::Command& command)
 	{
-		switch (command)
+		auto iter = commandHandlers.find(command);
+		if (iter != commandHandlers.end())
 		{
-		case ::Command::UP:
-			graphics::Menus::Previous(LAYOUT_NAME, MENU_ID);
-			break;
-		case ::Command::DOWN:
-			graphics::Menus::Next(LAYOUT_NAME, MENU_ID);
-			break;
-		case ::Command::LEFT:
-			DecreaseItem();
-			break;
-		case ::Command::RIGHT:
-			IncreaseItem();
-			break;
-		case ::Command::BACK:
-		case ::Command::RED:
-			::application::UIState::Write(::UIState::MAIN_MENU);
-			break;
-		case ::Command::GREEN:
-			ActivateItem();
-			break;
+			iter->second();
 		}
 	}
 
-	static void UpdateMuteMenuItem()
+	static void UpdateMuteMenuItem(const Uint32&)
 	{
 		graphics::MenuItems::SetText(LAYOUT_NAME, MUTE_MENU_ITEM_ID, 
 			common::Audio::IsMuted() ? UNMUTE : MUTE);
 	}
 
-	static void UpdateSfxMenuItem()
+	static void UpdateSfxMenuItem(const Uint32&)
 	{
 		std::stringstream ss;
 		ss << "SFX Volume (" << common::Utility::ToPercentage(common::audio::Sfx::GetVolume(), MIX_MAX_VOLUME) << "%)";
 		graphics::MenuItems::SetText(LAYOUT_NAME, SFX_VOLUME_MENU_ITEM_ID, ss.str());
 	}
 
-	static void UpdateMuxMenuItem()
+	static void UpdateMuxMenuItem(const Uint32&)
 	{
 		std::stringstream ss;
 		ss << "MUX Volume (" << common::Utility::ToPercentage(common::audio::Mux::GetVolume(), MIX_MAX_VOLUME) << "%)";
 		graphics::MenuItems::SetText(LAYOUT_NAME, MUX_VOLUME_MENU_ITEM_ID, ss.str());
 	}
 
-	static void OnUpdate(const Uint32& ticks)
-	{
-		UpdateMuteMenuItem();
-		UpdateSfxMenuItem();
-		UpdateMuxMenuItem();
-	}
-
 	void Start()
 	{
 		::application::Command::SetHandler(::UIState::OPTIONS, OnCommand);
 		::application::Renderer::SetRenderLayout(::UIState::OPTIONS, LAYOUT_NAME);
-		::application::Update::AddHandler(::UIState::OPTIONS, OnUpdate);
+		::application::Update::AddHandler(::UIState::OPTIONS, UpdateMuteMenuItem);
+		::application::Update::AddHandler(::UIState::OPTIONS, UpdateSfxMenuItem);
+		::application::Update::AddHandler(::UIState::OPTIONS, UpdateMuxMenuItem);
 	}
 }
