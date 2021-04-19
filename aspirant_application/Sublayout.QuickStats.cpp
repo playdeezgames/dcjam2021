bc40326cdf5fec3f80d3a49f4d8c995ff5b33b3a
ernestpazera <ernestpazera@gmail.com>
2021-04-19 11:34:04 -0500

    cleaned up quickstats

--- aspirant_application/Sublayout.QuickStats.cpp
+++ aspirant_application/Sublayout.QuickStats.cpp
@@ -4,38 +4,44 @@
 #include "Graphics.Texts.h"
 #include "Application.Update.h"
 #include "Graphics.Images.h"
+#include <vector>
 namespace sublayout::QuickStats
 {
 	const std::string LAYOUT_NAME = "Sublayout.QuickStats";
 	const std::string HEALTH_IMAGE_ID = "HealthBar";
 	const std::string HUNGER_IMAGE_ID = "HungerBar";
+	const std::string HEALTH_SPRITE_PREFIX = "HealthBarForeground";
+	const std::string HUNGER_SPRITE_PREFIX = "HungerBarForeground";
 
 	static void UpdateHealth(const Uint32&)
 	{
 		std::stringstream ss;
-		ss << "HealthBarForeground" << game::avatar::Statistics::Read(game::avatar::Statistic::HEALTH);
+		ss << HEALTH_SPRITE_PREFIX << game::avatar::Statistics::Read(game::avatar::Statistic::HEALTH);
 		::graphics::Images::SetSprite(LAYOUT_NAME, HEALTH_IMAGE_ID, ss.str());
 	}
 
 	static void UpdateHunger(const Uint32&)
 	{
 		std::stringstream ss;
-		ss << "HungerBarForeground" << game::avatar::Statistics::Read(game::avatar::Statistic::HUNGER);
+		ss << HUNGER_SPRITE_PREFIX << game::avatar::Statistics::Read(game::avatar::Statistic::HUNGER);
 		::graphics::Images::SetSprite(LAYOUT_NAME, HUNGER_IMAGE_ID, ss.str());
 	}
 
+	const std::vector<::UIState> states =
+	{
+		::UIState::IN_PLAY_MAP,
+		::UIState::IN_PLAY_INVENTORY,
+		::UIState::IN_PLAY_STATUS,
+		::UIState::IN_PLAY_COMBAT,
+		::UIState::IN_PLAY_COMBAT_RESULT,
+	};
 
 	void Start()
 	{
-		::application::Update::AddHandler(::UIState::IN_PLAY_MAP, UpdateHealth);
-		::application::Update::AddHandler(::UIState::IN_PLAY_MAP, UpdateHunger);
-		::application::Update::AddHandler(::UIState::IN_PLAY_INVENTORY, UpdateHealth);
-		::application::Update::AddHandler(::UIState::IN_PLAY_INVENTORY, UpdateHunger);
-		::application::Update::AddHandler(::UIState::IN_PLAY_STATUS, UpdateHealth);
-		::application::Update::AddHandler(::UIState::IN_PLAY_STATUS, UpdateHunger);
-		::application::Update::AddHandler(::UIState::IN_PLAY_COMBAT, UpdateHealth);
-		::application::Update::AddHandler(::UIState::IN_PLAY_COMBAT, UpdateHunger);
-		::application::Update::AddHandler(::UIState::IN_PLAY_COMBAT_RESULT, UpdateHealth);
-		::application::Update::AddHandler(::UIState::IN_PLAY_COMBAT_RESULT, UpdateHunger);
+		for (auto state : states)
+		{
+			::application::Update::AddHandler(state, UpdateHealth);
+			::application::Update::AddHandler(state, UpdateHunger);
+		}
 	}
 }
