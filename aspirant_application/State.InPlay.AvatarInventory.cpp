2dd6d8d2c1478d41b1e34c1df3f536c7a3005b50
ernestpazera <ernestpazera@gmail.com>
2021-04-20 04:03:27 -0500

    avatar inventory state refactor

--- aspirant_application/State.InPlay.AvatarInventory.cpp
+++ aspirant_application/State.InPlay.AvatarInventory.cpp
@@ -10,43 +10,26 @@
 #include "Game.Avatar.Statistics.h"
 #include "Common.Audio.h"
 #include "Application.Sounds.h"
+#include "Common.Utility.h"
 namespace state::in_play::AvatarInventory
 {
 	const std::string LAYOUT_NAME = "State.InPlay.AvatarInventory";
 
+	const std::map<Command, std::function<void()>> commandHandlers =
+	{
+		{ ::Command::BACK, []() { application::UIState::Write(::UIState::LEAVE_PLAY); }},
+		{ ::Command::PREVIOUS, []() { graphics::AvatarInventory::ResetIndex(); application::UIState::Write(::UIState::IN_PLAY_FLOOR); }},
+		{ ::Command::NEXT, []() { graphics::AvatarInventory::ResetIndex(); application::UIState::Write(::UIState::IN_PLAY_STATUS); }},
+		{ ::Command::YELLOW, []() { graphics::AvatarInventory::ResetIndex(); application::UIState::Write(::UIState::IN_PLAY_STATUS); }},
+		{ ::Command::UP, graphics::AvatarInventory::PreviousIndex },
+		{ ::Command::DOWN, graphics::AvatarInventory::NextIndex },
+		{ ::Command::RED, []() { game::avatar::Items::Drop(graphics::AvatarInventory::GetItem()); }},
+		{ ::Command::GREEN, []() { common::audio::Sfx::Play(game::avatar::Items::Use(graphics::AvatarInventory::GetItem())); }}
+	};
+
 	static void OnCommand(const ::Command& command)
 	{
-		switch (command)
-		{
-		case ::Command::BACK:
-			application::UIState::Write(::UIState::LEAVE_PLAY);
-			break;
-		case ::Command::PREVIOUS:
-			graphics::AvatarInventory::ResetIndex();
-			application::UIState::Write(::UIState::IN_PLAY_FLOOR);
-			break;
-		case ::Command::NEXT:
-		case ::Command::YELLOW:
-			graphics::AvatarInventory::ResetIndex();
-			application::UIState::Write(::UIState::IN_PLAY_STATUS);
-			break;
-		case ::Command::UP:
-			graphics::AvatarInventory::PreviousIndex();
-			break;
-		case ::Command::DOWN:
-			graphics::AvatarInventory::NextIndex();
-			break;
-		case ::Command::RED:
-			game::avatar::Items::Drop(graphics::AvatarInventory::GetItem());
-			break;
-		case ::Command::GREEN:
-			auto sfx = game::avatar::Items::Use(graphics::AvatarInventory::GetItem());
-			if (sfx)
-			{
-				common::audio::Sfx::Play(sfx.value());
-			}
-			break;
-		}
+		common::Utility::Dispatch(commandHandlers, command);
 	}
 
 	void Start()
