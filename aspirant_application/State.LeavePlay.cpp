99364258ef9229808570caf1be9438aab756be12
ernestpazera <ernestpazera@gmail.com>
2021-04-23 04:27:26 -0500

    mouse works on the leave play screen

--- aspirant_application/State.LeavePlay.cpp
+++ aspirant_application/State.LeavePlay.cpp
@@ -5,10 +5,15 @@
 #include "Application.Renderer.h"
 #include "Common.Audio.h"
 #include "Common.Utility.h"
+#include "Application.MouseButtonUp.h"
+#include "Application.MouseMotion.h"
+#include "Graphics.Areas.h"
 namespace state::LeavePlay
 {
 	const std::string LAYOUT_NAME = "State.LeavePlay";
 	const std::string MENU_ID = "LeavePlay";
+	const std::string AREA_CONTINUE = "Continue";
+	const std::string AREA_ABANDON = "Abandon";
 
 	enum class LeavePlayItem
 	{
@@ -36,8 +41,39 @@ namespace state::LeavePlay
 		{ ::Command::RED, []() { ::application::UIState::Write(::UIState::MAIN_MENU); }}
 	};
 
+	static void SetCurrentMenuItem(LeavePlayItem item)
+	{
+		graphics::Menus::WriteValue(LAYOUT_NAME, MENU_ID, (int)item);
+	}
+
+	const std::map<std::string, LeavePlayItem> areaMenuItems =
+	{
+		{ AREA_CONTINUE,  LeavePlayItem::CONTINUE},
+		{ AREA_ABANDON,  LeavePlayItem::ABANDON}
+	};
+
+	static void OnMouseMotion(const common::XY<Sint32>& xy)//TODO: make an MouseMotionArea handler?
+	{
+		auto areas = graphics::Areas::Get(LAYOUT_NAME, xy);
+		for (auto& area : areas)
+		{
+			SetCurrentMenuItem(areaMenuItems.find(area)->second);
+		}
+	}
+
+	static void OnMouseButtonUp(const common::XY<Sint32>& xy, Uint8)//TODO: duplicated code with other menus
+	{
+		auto areas = graphics::Areas::Get(LAYOUT_NAME, xy);
+		if (!areas.empty())
+		{
+			ActivateItem();
+		}
+	}
+
 	void Start()
 	{
+		::application::MouseButtonUp::SetHandler(::UIState::LEAVE_PLAY, OnMouseButtonUp);
+		::application::MouseMotion::AddHandler(::UIState::LEAVE_PLAY, OnMouseMotion);
 		::application::Command::SetHandlers(::UIState::LEAVE_PLAY, commandHandlers);
 		::application::Renderer::SetRenderLayout(::UIState::LEAVE_PLAY, LAYOUT_NAME);
 	}
