0135347e76b934414191397c06f1405114c5106b
ernestpazera <ernestpazera@gmail.com>
2021-04-23 04:47:03 -0500

    made the hamburger fully work on the minimap screen

--- aspirant_application/State.InPlay.MiniMap.cpp
+++ aspirant_application/State.InPlay.MiniMap.cpp
@@ -13,28 +13,75 @@
 #include <sstream>
 #include "States.h"
 #include "Common.Utility.h"
+#include "Application.MouseButtonUp.h"
+#include "Application.MouseMotion.h"
+#include "Graphics.Areas.h"
 namespace state::in_play::MiniMap
 {
 	const std::string LAYOUT_NAME = "State.InPlay.MiniMap";
+	const std::string AREA_UI_HAMBURGER = "UIHamburger";
+	const std::string IMAGE_UI_HAMBURGER = "ui-hamburger";
+	const std::string SPRITE_UI_HAMBURGER_NORMAL = "UIHamburgerNormal";
+	const std::string SPRITE_UI_HAMBURGER_HOVER = "UIHamburgerHover";
+
+	static void LeavePlay()
+	{
+		application::UIState::Write(::UIState::LEAVE_PLAY);
+	}
+
+	static void GoToFloorInventory()
+	{
+		application::UIState::Write(::UIState::IN_PLAY_FLOOR);
+	}
+
+	static void GoToAvatarStatus()
+	{
+		application::UIState::Write(::UIState::IN_PLAY_STATUS);
+	}
 
 	const std::map<::Command, std::function<void()>> commandHandlers =
 	{
-		{ ::Command::BACK, []() { application::UIState::Write(::UIState::LEAVE_PLAY); }},
+		{ ::Command::BACK, LeavePlay },
 		{ ::Command::LEFT, game::Avatar::TurnLeft },
 		{ ::Command::RIGHT, game::Avatar::TurnRight },
 		{ ::Command::UP, game::Avatar::MoveAhead },
 		{ ::Command::DOWN, game::Avatar::MoveBack },
-		{ ::Command::NEXT, []() { application::UIState::Write(::UIState::IN_PLAY_FLOOR); }},
-		{ ::Command::YELLOW, []() {	application::UIState::Write(::UIState::IN_PLAY_FLOOR); }},
-		{ ::Command::PREVIOUS, []() { application::UIState::Write(::UIState::IN_PLAY_STATUS); }}
+		{ ::Command::NEXT, GoToFloorInventory },
+		{ ::Command::YELLOW, GoToFloorInventory },
+		{ ::Command::PREVIOUS, GoToAvatarStatus }
 	};
 
 	static void OnCommand(const ::Command& command)
 	{
 		common::Utility::Dispatch(commandHandlers, command);
 	}
+
+	static void OnMouseMotion(const common::XY<Sint32>& xy)
+	{
+		auto areas = graphics::Areas::Get(LAYOUT_NAME, xy);
+		if (areas.contains(AREA_UI_HAMBURGER))
+		{
+			graphics::Images::SetSprite(LAYOUT_NAME, IMAGE_UI_HAMBURGER, SPRITE_UI_HAMBURGER_HOVER);
+		}
+		else
+		{
+			graphics::Images::SetSprite(LAYOUT_NAME, IMAGE_UI_HAMBURGER, SPRITE_UI_HAMBURGER_NORMAL);
+		}
+	}
+
+	static void OnMouseButtonUp(const common::XY<Sint32>& xy, Uint8)
+	{
+		auto areas = graphics::Areas::Get(LAYOUT_NAME, xy);
+		if (areas.contains(AREA_UI_HAMBURGER))
+		{
+			LeavePlay();
+		}
+	}
+
 	void Start()
 	{
+		::application::MouseButtonUp::SetHandler(::UIState::IN_PLAY_MAP, OnMouseButtonUp);
+		::application::MouseMotion::AddHandler(::UIState::IN_PLAY_MAP, OnMouseMotion);
 		::application::Command::SetHandlers(::UIState::IN_PLAY_MAP, commandHandlers);
 		::application::Renderer::SetRenderLayout(::UIState::IN_PLAY_MAP, LAYOUT_NAME);
 	}
