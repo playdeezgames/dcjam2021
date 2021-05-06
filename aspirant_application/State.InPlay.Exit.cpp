8ce1b8f221986935d076d8fadbaedfff4a1d3ae9
ernestpazera <ernestpazera@gmail.com>
2021-05-06 05:04:09 -0500

    refactored the exit state

--- aspirant_application/State.InPlay.Exit.cpp
+++ aspirant_application/State.InPlay.Exit.cpp
@@ -1,28 +1,26 @@
 #include "Application.Renderer.h"
 #include "Application.Command.h"
-#include "Application.Update.h"
+#include "Application.OnEnter.h"
 #include "Application.UIState.h"
 #include "Visuals.Texts.h"
 #include "Game.Avatar.Items.h"
 #include <sstream>
 #include "Application.MouseButtonUp.h"
+#include "Game.Avatar.h"
+#include "Visuals.Layouts.h"
 namespace state::in_play::Exit
 {
 	const std::string LAYOUT_NAME = "State.InPlay.Exit";
 	const std::string JOOLS_TEXT_ID = "Jools";
 	const std::string TROUSERS_TEXT_ID = "Jools";
 
-	static void GoToMainMenu()
+	static void OnEnter()
 	{
-		::application::UIState::Write(::UIState::MAIN_MENU);
-	}
-
-	static void OnUpdate(const Uint32&)
-	{
+		visuals::Layouts::InitializeLayout(LAYOUT_NAME);
 		std::stringstream ss;
-		ss << "You collected " << game::avatar::Items::Read(5) << " jools!";
+		ss << "You collected " << game::avatar::Items::Read(game::Avatar::GetDescriptor().joolsItemId) << " jools!";
 		visuals::Texts::SetText(LAYOUT_NAME, JOOLS_TEXT_ID, ss.str());
-		if (game::avatar::Items::Read(6) > 0)//TODO: hardcoded
+		if (game::avatar::Items::Read(game::Avatar::GetDescriptor().trousersItemId) > 0)
 		{
 			visuals::Texts::SetText(LAYOUT_NAME, TROUSERS_TEXT_ID, "...you managed to keep yer trousers!");
 		}
@@ -30,20 +28,19 @@ namespace state::in_play::Exit
 		{
 			visuals::Texts::SetText(LAYOUT_NAME, TROUSERS_TEXT_ID, "...you lost yer trousers(again)!");
 		}
-
 	}
 
 	bool OnMouseButtonUp(const common::XY<Sint32>& xy, Uint8 buttons)
 	{
-		GoToMainMenu();
+		::application::UIState::Write(::UIState::MAIN_MENU);
 		return true;
 	}
 
 	void Start()
 	{
 		::application::MouseButtonUp::AddHandler(::UIState::IN_PLAY_EXIT, OnMouseButtonUp);
-		::application::Command::SetHandler(::UIState::IN_PLAY_EXIT, GoToMainMenu);
+		::application::Command::SetHandler(::UIState::IN_PLAY_EXIT, ::application::UIState::GoTo(::UIState::MAIN_MENU));
 		::application::Renderer::SetRenderLayout(::UIState::IN_PLAY_EXIT, LAYOUT_NAME);
-		::application::Update::AddHandler(::UIState::IN_PLAY_EXIT, OnUpdate);
+		::application::OnEnter::AddHandler(::UIState::IN_PLAY_EXIT, OnEnter);
 	}
 }
