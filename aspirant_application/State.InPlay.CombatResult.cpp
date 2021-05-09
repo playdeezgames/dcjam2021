05e424d071fbce83afdae0436fe549cbf5dfa509
ernestpazera <ernestpazera@gmail.com>
2021-05-09 05:13:02 -0500

    cleaned up code for combatt result state

--- aspirant_application/State.InPlay.CombatResult.cpp
+++ aspirant_application/State.InPlay.CombatResult.cpp
@@ -1,25 +1,20 @@
 #include "Application.Command.h"
 #include "Application.Renderer.h"
-#include "Application.Update.h"
-#include "Application.UIState.h"
 #include "Game.Combat.h"
 #include "Visuals.Images.h"
-#include "Game.CombatDeck.h"
 #include "Visuals.CardSprites.h"
 #include "Common.Audio.h"
-#include "Common.Utility.h"
-#include "Visuals.Areas.h"
 #include "Application.MouseButtonUp.h"
 #include "Application.MouseMotion.h"
 #include "Visuals.Texts.h"
 #include "Visuals.Data.Colors.h"
 #include "Application.OnEnter.h"
-#include "Visuals.Layouts.h"
+#include "Visuals.Areas.h"
 namespace state::in_play::CombatResult
 {
 	const std::string LAYOUT_NAME = "State.InPlay.CombatResult";
-	const std::string CURRENT_CARD_IMAGE_ID = "CurrentCard";
-	const std::string NEXT_CARD_IMAGE_ID = "NextCard";
+	const std::string IMAGE_CURRENT_CARD = "CurrentCard";
+	const std::string IMAGE_NEXT_CARD = "NextCard";
 	const std::string AREA_CONTINUE = "Continue";
 	const std::string TEXT_CONTINUE = "Continue";
 
@@ -43,19 +38,18 @@ namespace state::in_play::CombatResult
 
 	static void OnEnter()
 	{
-		visuals::Images::SetSprite(LAYOUT_NAME, CURRENT_CARD_IMAGE_ID, visuals::CardSprites::GetSpriteForCard(game::CombatDeck::GetCurrentCard()));
-		visuals::Images::SetSprite(LAYOUT_NAME, NEXT_CARD_IMAGE_ID, visuals::CardSprites::GetSpriteForCard(game::CombatDeck::GetNextCard()));
+		visuals::Images::SetSprite(LAYOUT_NAME, IMAGE_CURRENT_CARD, visuals::CardSprites::GetSpriteForCard(game::CombatDeck::GetCurrentCard()));
+		visuals::Images::SetSprite(LAYOUT_NAME, IMAGE_NEXT_CARD, visuals::CardSprites::GetSpriteForCard(game::CombatDeck::GetNextCard()));
 	}
 
-	static void OnMouseMotion(const common::XY<Sint32>& xy)
+	static void OnMouseMotionInArea(const std::string& area)
 	{
-		auto areas = visuals::Areas::Get(LAYOUT_NAME, xy);
-		visuals::Texts::SetColor(LAYOUT_NAME, TEXT_CONTINUE, (areas.contains(AREA_CONTINUE)) ? (visuals::data::Colors::HIGHLIGHT) : (visuals::data::Colors::NORMAL));
+		visuals::Texts::SetColor(LAYOUT_NAME, TEXT_CONTINUE, (area == AREA_CONTINUE) ? (visuals::data::Colors::HIGHLIGHT) : (visuals::data::Colors::NORMAL));
 	}
 
-	static bool HandleContinue(const std::set<std::string>& areas)
+	static bool HandleContinue(const std::string& area)
 	{
-		if (areas.contains(AREA_CONTINUE))
+		if (area == AREA_CONTINUE)
 		{
 			AdvanceCombat();
 			return true;
@@ -63,16 +57,15 @@ namespace state::in_play::CombatResult
 		return false;
 	}
 
-	static bool OnMouseButtonUp(const common::XY<Sint32>& xy, Uint8)
+	static bool OnMouseButtonUpInAreaa(const std::string& area)
 	{
-		auto areas = visuals::Areas::Get(LAYOUT_NAME, xy);
-		return HandleContinue(areas);
+		return HandleContinue(area);
 	}
 
 	void Start()
 	{
-		application::MouseButtonUp::AddHandler(::UIState::IN_PLAY_COMBAT_RESULT, OnMouseButtonUp);
-		application::MouseMotion::AddHandler(::UIState::IN_PLAY_COMBAT_RESULT, OnMouseMotion);
+		application::MouseButtonUp::AddHandler(::UIState::IN_PLAY_COMBAT_RESULT, visuals::Areas::HandleMouseButtonUp(LAYOUT_NAME, OnMouseButtonUpInAreaa));
+		application::MouseMotion::AddHandler(::UIState::IN_PLAY_COMBAT_RESULT, visuals::Areas::HandleMouseMotion(LAYOUT_NAME, OnMouseMotionInArea));
 		::application::Command::SetHandlers(::UIState::IN_PLAY_COMBAT_RESULT, commandHandlers);
 		::application::Renderer::SetRenderLayout(::UIState::IN_PLAY_COMBAT_RESULT, LAYOUT_NAME);
 		::application::OnEnter::AddHandler(::UIState::IN_PLAY_COMBAT_RESULT, OnEnter);
