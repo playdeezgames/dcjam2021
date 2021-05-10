78ee10b1ac675b650c69e1cdf1952293289e6cbc
ernestpazera <ernestpazera@gmail.com>
2021-05-10 05:09:19 -0500

    refactored splash and did include cleanse

--- aspirant_application/State.Splash.cpp
+++ aspirant_application/State.Splash.cpp
@@ -1,9 +1,7 @@
 #include "Application.Renderer.h"
 #include "Application.Command.h"
 #include "Application.Update.h"
-#include "Application.UIState.h"
 #include "Application.MouseButtonUp.h"
-#include "Visuals.Areas.h"
 namespace state::Splash
 {
 	const size_t TICKS_TOTAL = 3000;
